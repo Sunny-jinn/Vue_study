13bf45f5dc2a38bdff00cb23e4045fc4f492e1e5
Sunny-jinn <rlawlsdn316@gmail.com>
2021-08-09 16:26:34 +0900

    이벤트 예제

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <a>Hello</a>
+    <h2 @click="num_incre += 1">{{num_incre}}</h2>
+  </div>
+  <div>
+    <a>{{인사}}</a>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      num_incre : 0,
+      인사 : "Hello",
+      num : [1,2,3,4]
+    }
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
