f1019962dc6e8bb0031b1f6e771fd9fa3c269f5b
비누 <rlawlsdn316@gmail.com>
2021-08-09 21:20:01 +0900

    클릭하면 친구들 얼굴 띄우기

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    <a>Hello</a>
-    <h2 @click="num_incre += 1">{{num_incre}}</h2>
+  <div class="banner">
+    <a @click="sub_name='Kim'">김영진</a>
+    <a @click="sub_name='Babo'">이현빈</a>
+    <a @click="sub_name='Ilbe'">이병진</a>
+    <a @click="sub_name='Choi'">최수완</a>
+    <a @click="sub_name='Yoon'">윤상필</a>
   </div>
   <div>
-    <a>{{인사}}</a>
+    <br /> <br />
+    <img src="./assets/Kim.png" v-if="sub_name == 'Kim' ">
+    <img src="./assets/Babo.png" v-if="sub_name == 'Babo'">
+    <img src="./assets/Ilbe.png" v-if="sub_name == 'Ilbe'">
+    <img src="./assets/Choi.png" v-if="sub_name == 'Choi'">
+    <img src="./assets/Yoon.png" v-if="sub_name == 'Yoon'">
   </div>
+
 </template>
 
 <script>
@@ -13,9 +22,9 @@
 export default {
   data() {
     return {
-      num_incre : 0,
-      인사 : "Hello",
-      num : [1,2,3,4]
+      chk : false,
+      sub_name : "",
+
     }
     
   }
@@ -23,12 +32,55 @@ export default {
 </script>
 
 <style>
+
+img {
+  width: 500px; height: 500px;
+}
+
+.Kim {
+  color: red;
+}
+
+.Babo {
+  color: green;
+}
+.Ilbe {
+  color: skyblue;
+}
+.Choi {
+  color: blueviolet;
+}
+.Yoon {
+  color: pink;
+}
+
+body {
+  margin: 0px;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.banner {
+  padding: 15px;
+  border-radius: 10px;
+  background:yellowgreen;
+  
+}
+
+.banner a {
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
