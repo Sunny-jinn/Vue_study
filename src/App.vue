0b43947519a238a90cc55b5fd306c7869ad55619
Sunny-jinn <rlawlsdn316@gmail.com>
2021-08-10 11:45:59 +0900

    css추가

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="banner">
-    <a @click="sub_name='Kim'">김영진</a>
-    <a @click="sub_name='Babo'">이현빈</a>
-    <a @click="sub_name='Ilbe'">이병진</a>
-    <a @click="sub_name='Choi'">최수완</a>
-    <a @click="sub_name='Yoon'">윤상필</a>
+    <a @click="sub_name='Kim'" :class="{salmon : sub_name == 'Kim'}">김영진</a>
+    <a @click="sub_name='Babo'" :class="{salmon : sub_name == 'Babo'}">이현빈</a>
+    <a @click="sub_name='Ilbe'" :class="{salmon : sub_name == 'Ilbe'}">이병진</a>
+    <a @click="sub_name='Choi'" :class="{salmon : sub_name == 'Choi'}">최수완</a>
+    <a @click="sub_name='Yoon'" :class="{salmon : sub_name == 'Yoon'}">윤상필</a>
   </div>
+  <button class="btn1"> ㅎㅇ</button>
   <div>
     <br /> <br />
     <img src="./assets/Kim.png" v-if="sub_name == 'Kim' ">
@@ -37,22 +38,6 @@ img {
   width: 500px; height: 500px;
 }
 
-.Kim {
-  color: red;
-}
-
-.Babo {
-  color: green;
-}
-.Ilbe {
-  color: skyblue;
-}
-.Choi {
-  color: blueviolet;
-}
-.Yoon {
-  color: pink;
-}
 
 body {
   margin: 0px;
@@ -75,6 +60,39 @@ div {
   font-weight: bold;
   padding: 10px;
 }
+
+.salmon {
+  color: lightcoral !important;
+}
+
+.banner a:hover {
+  color: lightcoral;
+}
+
+.btn1 {
+  background: white;
+  border: 0px;
+  border-radius: 20px;
+  padding : 3px;
+  float: right;
+  position: relative;
+  right: 12px;
+  bottom: 40px;
+  box-shadow: 1px 1px 1px lightslategray;
+  transition-duration: 0.3s;
+}
+
+.btn1:hover {
+  background: lightgrey;
+}
+
+.btn1:active {
+  margin-left:1px;
+  margin-top:1px;
+  box-shadow: none;
+}
+
+
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
